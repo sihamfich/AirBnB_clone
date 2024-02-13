c49a215a25c63d489f285221bae5332084e3d2bb
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-02-13 15:55:19 -0800

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__,self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
@@ -29,14 +29,3 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
-if __name__ == "__main__":
-    my_model = BaseModel(name="My First Model", my_number=89, created_at='2024-01-29T10:15:30.123456', updated_at='2024-01-29T11:20:35.987654')
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
