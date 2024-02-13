fbf901202990be36da98ffc3be66018ee2db9096
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-02-13 15:40:51 -0800

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 
-
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
-
-    def _init__(self):
-        self.id = str(uuid.uuid4())
-        self_created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updates_at']:
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}"\
-                .format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
@@ -25,3 +29,14 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+
+if __name__ == "__main__":
+    my_model = BaseModel(name="My First Model", my_number=89, created_at='2024-01-29T10:15:30.123456', updated_at='2024-01-29T11:20:35.987654')
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,8 +1,44 @@
 #!/usr/bin/python3
-from base_model import BaseModel
 import unittest
+from  models.base_model import BaseModel
+from datetime import datetime
 
 
-class test_base_model(TestCase):
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        # Initialize any objects or variables needed for testing
+        self.obj = BaseModel()
 
-    l
+    def tearDown(self):
+        # Clean up after each test
+        pass
+
+    def test_id_is_string(self):
+        self.assertIsInstance(self.obj.id, str)
+
+    def test_created_at_is_datetime(self):
+        self.assertIsInstance(self.obj.created_at, datetime)
+
+    def test_updated_at_is_datetime(self):
+        self.assertIsInstance(self.obj.updated_at, datetime)
+
+    def test_save_updates_updated_at(self):
+        old_updated_at = self.obj.updated_at
+        self.obj.save()
+        new_updated_at = self.obj.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+
+    def test_to_dict_returns_dict(self):
+        obj_dict = self.obj.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+
+    def test_to_dict_contains_class_name(self):
+        obj_dict = self.obj.to_dict()
+        self.assertIn('__class__', obj_dict)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+
+    # Add more test methods as needed
+
+if __name__ == '__main__':
+    unittest.main()
+
