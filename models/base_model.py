22c359e300167c2744cb045ed224c2b8e29c4be6
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-20 16:43:34 -0700

    task 3 fix

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,10 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -14,21 +18,22 @@ class BaseModel:
                                 strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                     else:
                         setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(type(self).__name__,
+        """
+        return string representation of the class
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
+        obj_dict: dict = {}
+        for key, value in self.__dict__.items():
+            if isinstance(value, datetime):
+                value = value.isoformat()
+            obj_dict[key] = value
         obj_dict['__class__'] = type(self).__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
